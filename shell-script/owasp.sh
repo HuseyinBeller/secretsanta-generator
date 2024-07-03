e23d19aa240470005967f581c47500b81cd3b893
Hüseyin Beller <77902440+HuseyinBeller@users.noreply.github.com>
2024-07-03 18:47:13 +0300

    Update owasp.sh

--- shell-script/owasp.sh
+++ shell-script/owasp.sh
@@ -1,3 +1,6 @@
 #/bin/bash
+DEPENDENCY_CHECK_VERSION = '6.5.1'
+DEPENDENCY_CHECK_URL = "https://github.com/jeremylong/DependencyCheck/releases/download/v${DEPENDENCY_CHECK_VERSION}/dependency-check-${DEPENDENCY_CHECK_VERSION}-release.zip"
+DEPENDENCY_CHECK_HOME = "${env.WORKSPACE}/dependency-check"
 curl -L ${DEPENDENCY_CHECK_URL} -o dependency-check.zip; unzip dependency-check.zip -d ${DEPENDENCY_CHECK_HOME};rm dependency-check.zip
                   
