cd8f5f16c5e312a45a0b1f8f597d1c0c9eebe359
Hüseyin Beller <77902440+HuseyinBeller@users.noreply.github.com>
2024-07-03 19:52:53 +0300

    Update trivy.sh

--- shell-script/trivy.sh
+++ shell-script/trivy.sh
@@ -1,2 +1,2 @@
-sh 'chmod u+x $WORKSPACE/shell-script/trivy.sh'
+sh 'chmod u+x $WORKSPACE/shell-script/trivy.sh;$WORKSPACE/shell-script/trivy.sh'
 sh 'trivy image --format table -o trivy-report.html huseyinbeller/secret-santa:latest'
